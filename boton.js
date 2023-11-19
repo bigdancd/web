bdea03c4f775e73395569cd0c9cd971d14e47e23
bigdancd <151419195+bigdancd@users.noreply.github.com>
2023-11-19 06:20:34 -0600

    Add files via upload

---
+++ boton.js
@@ -0,0 +1,7 @@
+function myFunction() {
+    alert("Gracias por visitar mi CV!");
+  }
+  function cambiarColor(){
+    var selectValue =       document.getElementById("colorch").value;   document.body.style.backgroundColor=selectValue;
+  }
+  
